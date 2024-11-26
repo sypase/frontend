35f05601010f918b83a6cdd2261b872c3c214721
Joyash <Anonymous526718@gmail.com>
2024-11-26 18:47:05 +0545

    a

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -156,7 +156,7 @@ export default function Home() {
     getUser();
 
     if (latestMessageRef.current) {
-      // latestMessageRef.current.scrollIntoView({ behavior: "smooth" });
+      latestMessageRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [messages]);
 
